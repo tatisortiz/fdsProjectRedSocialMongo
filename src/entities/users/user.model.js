c51ff9c961f0e93a9c88617322a077d02c6bfd60
tatianaD <angietatiana167@gmail.com>
2024-07-15 18:07:24 +0200

    feat: users controllers create

--- src/entities/users/user.model.js
+++ src/entities/users/user.model.js
@@ -1,36 +1,45 @@
 import { Schema, model } from "mongoose";
 
 const Usershema = new Schema(
-{ 
-   email:{
-    type: String,
-    required: true,
-    unique:true
-   },
+    {
+        first_name: {
+            type: String,
+            required: true
+        },
 
-   password:{
-    type: String,
-    required: true,
+        last_name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
 
-   },
+        password: {
+            type: String,
+            required: true,
 
-   is_active: {
-    type: Boolean,
-    default: true,
-  },
+        },
 
-   role: {
-  type: String,
-  Enum: ["user","admin","super_admin"],
-  default: "user"
-  },
-},
-{
-    timestamps: true,
-   
-}
+        is_active: {
+            type: Boolean,
+            default: true,
+        },
+
+        role: {
+            type: String,
+            Enum: ["user", "admin", "super_admin"],
+            default: "user"
+        },
+    },
+    {
+        timestamps: true,
+
+    }
 );
 
-const User = model('User',Usershema);
+const User = model('User', Usershema);
 
 export default User;
--- src/entities/users/users.controller.js
+++ src/entities/users/users.controller.js
@@ -5,9 +5,27 @@ import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
     try {
+        const { first_name, last_name, email, password} = req.body
         
-
-
+        const hashedPassword=  bcrypt.hashSync(password, parseInt(process.env.SALT_ROUNDS))
+        
+        const newUser= await User.create (
+            {   
+                first_name : first_name,
+                last_name: last_name,
+                email: email,
+                password: hashedPassword
+            }
+        )
+      
+    res.status(200).json(
+        {
+            success:true,
+            message: "User registered",
+            data: newUser
+        }
+    )
+   
 
     } catch (error) {
         res.status(500).json(
@@ -20,3 +38,4 @@ export const register = async (req, res) => {
         
     }
 }
+
