4eb954545e9f4ec58e7cbeafa46da357880ebc35
tatianaD <angietatiana167@gmail.com>
2024-07-15 16:54:11 +0200

    server running7

---
+++ src/database/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+    return mongoose.connect(
+        process.env.MONGO_URI);
+
+};
---
+++ src/server.js
@@ -0,0 +1,37 @@
+import  express from 'express';
+import 'dotenv/config';
+import { dbConnection } from './database/db.js';
+
+const app = express()
+
+const PORT =process.env.PORT || 4000
+
+
+app.use(express.json())
+
+
+app.get('/healthy',(req, res)=> {
+    res.json(
+        {
+            success: true,
+            message:"Server is healthy"
+        }
+    )
+})
+
+
+
+
+
+
+ dbConnection().then(()=> {
+    console.log('Databse connected');
+   app.listen(PORT, () => {
+        console.log(`server runnig ${PORT}`);
+     })
+ })
+
+ .catch (error => {
+    console.log('Error connection database:'+ error);
+ })
+
