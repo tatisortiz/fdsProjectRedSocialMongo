47316f97f1edc90ca4fa6d63f92d40fcb32844eb
tatianaD <angietatiana167@gmail.com>
2024-07-16 16:57:33 +0200

    feat: middleware auth create

---
+++ src/entities/middlewares/auth.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+
+export const auth = (req, res, next) => {
+  try {
+    if(!req.headers.authorization) {
+      return res.status(401).json(
+        {
+          suucess: false,
+          message: "Unauthorized"
+        }
+      )
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);    
+     console.log(decoded)
+    req.tokenData = {
+      id: decoded._id,
+      role: decoded.role
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json(
+      {
+        success: false,
+        message: "Error authenticadd",
+        error: error
+      }
+    )
+  }
+}
