b3ddd15cf748db4f71f7d82fdf882244f685192e
tatianaD <angietatiana167@gmail.com>
2024-07-20 21:35:35 +0200

    feat: create controller deletepostByIdAdmin

--- src/entities/posts/post.controller.js
+++ src/entities/posts/post.controller.js
@@ -1,268 +1,290 @@
 import { Types } from "mongoose";
-import Post from "./post.model.js"
-
+import Post from "./post.model.js";
 
 export const createPost = async (req, res) => {
-    try {
-        const title = req.body.title;
-        const description = req.body.description;
-        const userId = req.tokenData.id;
+  try {
+    const { title, description } = req.body;
+    const userId = req.tokenData.id;
 
-        const newPost = await Post.create({
-            title: title,
-            description: description,
-            user_id: userId
-        });
+    const newPost = await Post.create({
+      title: title,
+      description: description,
+      user_id: userId,
+    });
 
-        const populatedPost = await Post.findById(newPost._id).populate({
-            path: "user_id",
-            select: "-password"
-        });
+    const populatedPost = await Post.findById(newPost._id).populate({
+      path: "user_id",
+      select: "-password",
+    });
 
-
-        res.status(201).json({
-            success: true,
-            message: "New post created succesfully",
-            data: populatedPost
-        });
-
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: "error creating new post",
-            error: error.message,
-        });
-
-    }
-}
-
-export const deletePostById = async (req, res) => {
-    try {
-         const userId = req.tokenData.id;
-        const postIdToDelete = req.params.id;
-        const postIdToDeleteIsValid = Types.ObjectId.isValid(postIdToDelete);
-        if (!postIdToDeleteIsValid) {
-          return res.status(400).json({
-            success: false,
-            message: "Post id not valid",
-          });
-        }
-        const post = await Post.findById(postIdToDelete);
-
-       if (post.user_id.toString() !== req.tokenData.id) {
-          return res.status(403).json({
-            success: false,
-            message: "You are not authorized to delete this post",
-          });
-        }
-        const deletedPost = await Post.findByIdAndDelete(postIdToDelete);
-        if (!deletedPost) {
-          return res.status(404).json({
-            succes: false,
-            message: "Post not found",
-          });
-        }
-        res.status(200).json({
-          success: true,
-          message: "Post deleted",
-        });
-      } catch (error) {
-        return res.status(500).json({
-          success: false,
-          message: "Error trying to delete post",
-          error: error.message,
-        });
-      }
-    };
-
-
-    export const updatePostById = async (req, res) => {
-        try {
-            const Id = req.params.id
-            const { title, description } = req.body
-    
-            const post = await Post.findOne({ _id: Id })
-    
-            if (!post) {
-                return res.status(404).json({  
-                    success: false,
-                    message: "Post does not exist"
-                })
-            }
-    
-            if (post.user_id.toString() !== req.tokenData.id) {
-                return res.status(403).json({
-                  success: false,
-                  message: "You are not authorized to update this post",
-                });
-            }
-    
-            const updatedPost = await Post.updateOne(
-                { _id: Id },
-                { title: title, description: description }
-            )
-    
-            res.status(201).json({
-                success: true,
-                message: "Post updated",
-                data: updatedPost
-            })
-    
-        } catch (error) {
-            res.status(500).json({
-                success: false,
-                message: "Error updating post"
-            })
-        }
-    }
-
-export const getPostOwn = async (req, res) => {
-    
-    try {
-
-        const userId = req.tokenData.id;
-
-        const posts = await Post.find({ user_id: userId })
-
-        if (posts.length === 0) {
-            return res.status(404).json({
-                success: false,
-                message: "You haven't created any posts yet",
-            });
-        }
-
-        res.status(200).json({
-            success: true,
-            message: "Your posts retrieved",
-            data: posts
-        });
-
-        
-    } catch (error) {
-        res.status(500).json(
-            {
-                success: false,
-                message: "Can't retrieve your posts",
-                error: error.message
-            }
-        )
-        
-    }
+    res.status(201).json({
+      success: true,
+      message: "New post created succesfully",
+      data: populatedPost,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error trying to create new post",
+      error: error.message,
+    });
+  }
 };
 
-export const getALLPost = async (req, res) => {
-        try {
-            const posts = await Post.find().populate({ path: "user_id", select: "first_name last_name" });
-    
-            if (posts.length === 0) {
-                return res.status(404).json({
-                    success: false,
-                    message: "No posts found",
-                });
-            }
-    
-            res.status(200).json({
-                success: true,
-                message: "Posts retrieved",
-                data: posts,
-            });
-    
-        } catch (error) {
-            res.status(500).json({
-                success: false,
-                message: "Posts can't be retrieved",
-                error: error.message,
-            });
-        }
+export const deletePostById = async (req, res) => {
+  try {
+    const userId = req.tokenData.id;
+    const postIdToDelete = req.params.id;
+
+    const postIdToDeleteIsValid = Types.ObjectId.isValid(postIdToDelete);
+    if (!postIdToDeleteIsValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Post id not valid",
+      });
     }
+    const post = await Post.findById(postIdToDelete);
+
+    if (post.user_id.toString() !== userId) {
+      return res.status(403).json({
+        success: false,
+        message: "You can only delete your own posts",
+      });
+    }
+
+    await Post.findByIdAndDelete(postIdToDelete);
+
+    res.status(200).json({
+      success: true,
+      message: "Post deleted",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error trying to delete post",
+      error: error.message,
+    });
+  }
+};
+
+export const deletePostByIdAdmin = async (req, res) => {
+  try {
+    const postIdToDelete = req.params.id;
+
+    const postIdToDeleteIsValid = Types.ObjectId.isValid(postIdToDelete);
+    if (!postIdToDeleteIsValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Post id not valid",
+      });
+    }
+
+    await Post.findByIdAndDelete(postIdToDelete);
+
+    res.status(200).json({
+      success: true,
+      message: "Post deleted",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error trying to delete post",
+      error: error.message,
+    });
+  }
+};
+
+export const updatePostById = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const userId = req.tokenData.id;
+    const { title, description } = req.body;
+
+    const post = await Post.findOne({ _id: postId });
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    if (post.user_id.toString() !== userId) {
+      return res.status(403).json({
+        success: false,
+        message: "You can only update your own posts",
+      });
+    }
+
+    const updatedPost = await Post.updateOne(
+      { _id: postId },
+      { title: title, description: description }
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Post updated",
+      data: updatedPost,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error trying to update post",
+    });
+  }
+};
+
+export const getPostOwn = async (req, res) => {
+  try {
+    const userId = req.tokenData.id;
+
+    const posts = await Post.find({ user_id: userId });
+
+    if (posts.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "You haven't created any posts yet",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Your posts retrieved",
+      data: posts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error trying to retrieve your posts",
+      error: error.message,
+    });
+  }
+};
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().populate({
+      path: "user_id",
+      select: "first_name last_name",
+    });
+
+    if (posts.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No posts found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "All posts retrieved",
+      data: posts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error trying to retrieve all posts",
+      error: error.message,
+    });
+  }
+};
+
 export const getPostById = async (req, res) => {
-    try {
-    
-                const postId = req.params.id;
-        
-                const post = await Post.findById(postId).populate({ path: "user_id", select: "-password " });
-        
-                if (!post) {
-                    return res.status(404).json({
-                        success: false,
-                        message: "Post not found"
-                    });
-                }
-        
-                res.status(200).json({
-                    success: true,
-                    message: "Post retrieved successfully",
-                    data: post
-                });
-        
-            } catch (error) {
-                res.status(500).json({
-                    success: false,
-                    message: "Error retrieving post",
-                    error: error.message
-                });
-            } 
-}
+  try {
+    const postId = req.params.id;
+
+    const post = await Post.findById(postId).populate({
+      path: "user_id",
+      select: "-password ",
+    });
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Post retrieved successfully",
+      data: post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error trying to retrieve post",
+      error: error.message,
+    });
+  }
+};
 
 export const getPostsByUserId = async (req, res) => {
-    try {
-      const Id = req.params.id;
-  
-      const posts = await Post.find({ user_id: Id });
-  
-      if (posts.length === 0) {
-        return res.status(404).json({
-          success: false,
-          message: "No posts by this user",
-        });
-      }
-  
-      res.status(201).json({
-        success: true,
-        message: "Post(s) found",
-        data: posts,
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: "Error finding post by id",
-      });
-    }
-  };
+  try {
+    const userId = req.params.id;
 
-  export const likeUserById= async (req, res) => {
-    try {
-      const userId = req.tokenData.id;
-      const postId = req.params.id;
-      console.log(userId);
-      const post = await Post.findById(postId);
-      if (!post) {
-        return res.status(404).json({
-          success: false,
-          message: "Post not found",
-        });
-      }
-      const hasLike = post.likes.includes(userId);
-      if (hasLike) {
-        post.likes.pull(userId);
-      } else {
-        post.likes.push(userId);
-      }
-      await post.save();
-      const message = hasLike
-        ? "Post unliked successfully"
-        : "Post liked successfully";
-      res.status(200).json({
-        success: true,
-        message: message,
-        data: post,
-      });
-    } catch (error) {
-      res.status(500).json({
+    const posts = await Post.find({ user_id: userId });
+
+    if (posts.length === 0) {
+      return res.status(404).json({
         success: false,
-        message: "Error liking/unliking post",
-        error: error.message,
+        message: "No posts by this user",
       });
     }
-  };
+
+    res.status(201).json({
+      success: true,
+      message: "Post(s) found",
+      data: posts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error trying to find post by id",
+    });
+  }
+};
+
+export const likePostById = async (req, res) => {
+  try {
+    const userId = req.tokenData.id;
+    const postId = req.params.id;
+
+    console.log(userId);
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    const hasLike = post.likes.includes(userId);
+
+    if (hasLike) {
+      post.likes.pull(userId);
+    } else {
+      post.likes.push(userId);
+    }
+
+    await post.save();
+
+    const message = hasLike
+      ? "Post unliked successfully"
+      : "Post liked successfully";
+
+    res.status(200).json({
+      success: true,
+      message: message,
+      data: post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error liking/unliking post",
+      error: error.message,
+    });
+  }
+};
--- src/entities/posts/post.routes.js
+++ src/entities/posts/post.routes.js
@@ -1,15 +1,17 @@
 import { Router } from 'express'
-import { createPost, deletePostById, getALLPost, getPostById, getPostOwn, getPostsByUserId, likeUserById, updatePostById } from './post.controller.js'
+import { createPost, deletePostById, deletePostByIdAdmin, getAllPosts, getPostById, getPostOwn, getPostsByUserId, likePostById, updatePostById } from './post.controller.js'
 import { auth } from '../middlewares/auth.js'
+import { isSuperAdmin} from '../middlewares/isSuperAdmin.js'
 
 const router = Router()
 
 router.post('/',auth,createPost)
+router.delete("/admin/:id", auth, isSuperAdmin, deletePostByIdAdmin);
 router.delete('/:id', auth, deletePostById)
-router.put('/like/:id',auth, likeUserById)
+router.put('/like/:id',auth, likePostById)
 router.put('/:id', auth ,updatePostById)
 router.get('/own', auth, getPostOwn)
-router.get('/',auth, getALLPost)
+router.get('/',auth, getAllPosts)
 router.get("/user/:id", getPostsByUserId);
 router.get('/:id',auth , getPostById)
 
