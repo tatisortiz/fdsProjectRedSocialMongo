4ba9b66fe022d23c123af1395a0ace4f4a5c2427
Andersson <angietatiana167@gmail.com>
2024-08-29 19:21:40 -0500

    feat: CORS enabled

--- src/server.js
+++ src/server.js
@@ -2,12 +2,21 @@ import  express from 'express';
 import 'dotenv/config';
 import { dbConnection } from './database/db.js';
 import { router } from './router.js';
+import cors from 'cors';
 
 const app = express()
 
 app.use(express.json());
 
 const PORT =process.env.PORT || 4000
+
+
+app.use(cors({
+    origin: 'http://localhost:5174',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
 
 app.use(express.json())
 app.get('/healthy',(req, res)=> {
